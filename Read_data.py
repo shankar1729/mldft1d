87916dcea8633294aba0968fbdee42409b5b1bcb
Qihan <fuqihan777@gmail.com>
2022-10-19 21:10:10 -0400

    simplified version

--- Read_data.py
+++ Read_data.py
@@ -4,15 +4,14 @@ import numpy as np
 
 f = h5py.File('data.hdf5', 'r')
 
-z = f['z1d']
+z = f['z']
 V = f['V']
 P_lambda = f['lambda']
 n = f['n']
 E = f['E']
 
-plt.plot(z[0], V[0], label="V")
-plt.plot(z[0], n[50], label="n")
-plt.axhline(0.6, color='k', ls='dotted')
-plt.xlabel("z")
-plt.legend()
-plt.show()
+print(z.shape)
+print(V.shape)
+print(P_lambda.shape)
+print(n.shape)
+print(E.shape)
--- generate_data.py
+++ generate_data.py
@@ -11,14 +11,14 @@ qp.rc.init()
 def run(*,
         L: float,
         dz: float,
-        R: float,  #: Radius / half-length `R`
-        T: float, #: Temperature
-        n_bulk: float, #: Bulk number density of the fluid
+        R: float,           #: Radius / half-length `R`
+        T: float,           #: Temperature
+        n_bulk: float,      #: Bulk number density of the fluid
         Vsigma: float,
-        lbda: dict, #: min: float, max: float, step: float, 
-        filename: str #: hdf5 filename, must end with .hdf5
+        lbda: dict,         #: min: float, max: float, step: float, 
+        filename: str       #: hdf5 filename, must end with .hdf5
         ):
-        
+
     grid1d = Grid1D(L=L, dz=dz)
     cdft = HardRodsFMT(grid1d, R=R, T=T, n_bulk=n_bulk)
     
@@ -28,25 +28,23 @@ def run(*,
     n = []
     E = []
     P_lambda = np.arange(start = lbda['min'], stop = lbda['max'], step = lbda['step'], dtype = 'f')
+
     for V0 in P_lambda:
-        # cdft.V.data = (0.5 * V0) * ((0.4 * grid1d.L - (grid1d.z - 0.5 * grid1d.L).abs()) / Vsigma).erfc()
         cdft.V = V0 * Vshape
         cdft.finite_difference_test(cdft.random_direction())
-        E.append(cdft.minimize())
-        n.append(get1D(cdft.n.data))
+        E.append(float(cdft.minimize()))
+        n.append(np.array(get1D(cdft.n.data)))
 
 
     f = h5py.File(filename, "w")
 
-    f['z'] = get1D(grid1d.z)
-    f['V'] = get1D(Vshape.data)
-    f['lambda'] = P_lambda
-    f['n'] = np.array(n)
+    f['z'] = get1D(grid1d.z)            #: (# grid, )
+    f['V'] = get1D(Vshape.data)         #: (# grid, )
+    f['lambda'] = P_lambda              #: (# perturbation, )
+    f['n'] = np.array(n)                #: (# perturbation, # grid)
+    f["E"]= E                           #: (# perturbation, )
 
-    dset5 = f.create_dataset("E", (1,len(E)), dtype = 'f')
-    dset5[...] = E
-
-    f.attrs["n_bulk"] = 0.6 
+    f.attrs["n_bulk"] = n_bulk
 
     f.close()
     return 
