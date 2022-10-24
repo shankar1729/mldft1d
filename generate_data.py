602d2e4a18a60510a9920727620d6773dd741655
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2022-10-24 09:07:37 -0400

    Added precommit hooks and input file name to generate_data

--- generate_data.py
+++ generate_data.py
@@ -4,11 +4,7 @@ from grid1d import Grid1D, get1D
 from hard_rods_fmt import HardRodsFMT
 import h5py
 import yaml
-
-
-qp.utils.log_config()  # default set up to log from MPI head alone
-qp.log.info("Using QimPy " + qp.__version__)
-qp.rc.init()
+import sys
 
 
 def run(
@@ -31,25 +27,39 @@ def run(
         grid1d.grid, data=(-0.5 * ((grid1d.z - 0.5 * grid1d.L) / Vsigma).square()).exp()
     )
 
-    n = []
-    E = []
     lbda_arr = np.arange(lbda["min"], lbda["max"], lbda["step"])
+    n = []  # dimensions: len(lbda_arr), grid
+    E = []  # dimensions: len(lbda_arr)
     for lbda_cur in lbda_arr:
         cdft.V = lbda_cur * Vshape
         E.append(float(cdft.minimize()))
         n.append(np.array(get1D(cdft.n.data)))
 
     f = h5py.File(filename, "w")
-    f["z"] = get1D(grid1d.z)  #: (# grid, )
-    f["V"] = get1D(Vshape.data)  #: (# grid, )
-    f["lambda"] = lbda_arr  #: (# perturbation, )
-    f["n"] = np.array(n)  #: (# perturbation, # grid)
-    f["E"] = E  #: (# perturbation, )
+    f["z"] = get1D(grid1d.z)
+    f["V"] = get1D(Vshape.data)
+    f["lbda"] = lbda_arr
+    f["n"] = np.array(n)
+    f["E"] = E
     f.attrs["n_bulk"] = n_bulk
+    f.attrs["T"] = T
+    f.attrs["R"] = R
     f.close()
 
 
-with open(r"Params.yaml") as file:
-    Params = yaml.load(file, Loader=yaml.FullLoader)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python generate_data.py <input.yaml>")
+        exit(1)
+    in_file = sys.argv[1]
 
-run(**Params)
+    qp.utils.log_config()  # default set up to log from MPI head alone
+    qp.log.info("Using QimPy " + qp.__version__)
+    qp.rc.init()
+
+    with open(in_file) as fp:
+        run(**yaml.load(fp, Loader=yaml.FullLoader))
+
+
+if __name__ == "__main__":
+    main()
