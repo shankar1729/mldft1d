77485d693f63bc8875325d001c1e69a104cfc313
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2022-10-24 08:49:11 -0400

    Updated variable names and enforced format on generate_data.py.
    Moved examples input into anotehr directory

--- generate_data.py
+++ generate_data.py
@@ -3,55 +3,53 @@ import numpy as np
 from grid1d import Grid1D, get1D
 from hard_rods_fmt import HardRodsFMT
 import h5py
+import yaml
+
 
 qp.utils.log_config()  # default set up to log from MPI head alone
 qp.log.info("Using QimPy " + qp.__version__)
 qp.rc.init()
 
-def run(*,
-        L: float,
-        dz: float,
-        R: float,           #: Radius / half-length `R`
-        T: float,           #: Temperature
-        n_bulk: float,      #: Bulk number density of the fluid
-        Vsigma: float,
-        lbda: dict,         #: min: float, max: float, step: float, 
-        filename: str       #: hdf5 filename, must end with .hdf5
-        ):
+
+def run(
+    *,
+    L: float,
+    dz: float,
+    R: float,  #: Radius / half-length `R`
+    T: float,  #: Temperature
+    n_bulk: float,  #: Bulk number density of the fluid
+    Vsigma: float,
+    lbda: dict,  #: min: float, max: float, step: float,
+    filename: str,  #: hdf5 filename, must end with .hdf5
+) -> None:
 
     grid1d = Grid1D(L=L, dz=dz)
     cdft = HardRodsFMT(grid1d, R=R, T=T, n_bulk=n_bulk)
-    
+
     qp.log.info(f"mu = {cdft.mu}")
-    Vshape = qp.grid.FieldR(grid1d.grid, data=(-0.5 * ((grid1d.z - 0.5 * grid1d.L) / Vsigma).square()).exp())
+    Vshape = qp.grid.FieldR(
+        grid1d.grid, data=(-0.5 * ((grid1d.z - 0.5 * grid1d.L) / Vsigma).square()).exp()
+    )
 
     n = []
     E = []
-    P_lambda = np.arange(start = lbda['min'], stop = lbda['max'], step = lbda['step'], dtype = 'f')
-
-    for V0 in P_lambda:
-        cdft.V = V0 * Vshape
-        cdft.finite_difference_test(cdft.random_direction())
+    lbda_arr = np.arange(lbda["min"], lbda["max"], lbda["step"])
+    for lbda_cur in lbda_arr:
+        cdft.V = lbda_cur * Vshape
         E.append(float(cdft.minimize()))
         n.append(np.array(get1D(cdft.n.data)))
 
-
     f = h5py.File(filename, "w")
-
-    f['z'] = get1D(grid1d.z)            #: (# grid, )
-    f['V'] = get1D(Vshape.data)         #: (# grid, )
-    f['lambda'] = P_lambda              #: (# perturbation, )
-    f['n'] = np.array(n)                #: (# perturbation, # grid)
-    f["E"]= E                           #: (# perturbation, )
+    f["z"] = get1D(grid1d.z)  #: (# grid, )
+    f["V"] = get1D(Vshape.data)  #: (# grid, )
+    f["lambda"] = lbda_arr  #: (# perturbation, )
+    f["n"] = np.array(n)  #: (# perturbation, # grid)
+    f["E"] = E  #: (# perturbation, )
     f.attrs["n_bulk"] = n_bulk
-
     f.close()
-    return 
 
-import yaml
 
-with open(r'Params.yaml') as file:
+with open(r"Params.yaml") as file:
     Params = yaml.load(file, Loader=yaml.FullLoader)
 
 run(**Params)
-
