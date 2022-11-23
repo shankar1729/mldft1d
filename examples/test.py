66b8e4622542d21cfe807089a45a22667ea83a34
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2022-11-23 11:45:43 -0500

    Added comparison of exact and ML CDFT results in test.py

--- examples/test.py
+++ examples/test.py
@@ -10,46 +10,48 @@ def main():
     qp.rc.init()
 
     grid1d = hr.Grid1D(L=40.0, dz=0.01)
-    use_exact_cdft = False
+    n_bulk = 0.6
+    T = 1.0
 
-    if use_exact_cdft:
-        # Use exact functional:
-        cdft = hr.HardRodsFMT(grid1d, R=0.5, T=1.0, n_bulk=0.6)
-    else:
-        # Use MLCDFT approximation:
-        functional = hr.mlcdft.Functional(
-            T=1.0,
-            w=hr.mlcdft.NNFunction(1, 2, [10, 10]),
-            f_ex=hr.mlcdft.NNFunction(2, 2, [10, 10]),
-        )
-        functional.load_state_dict(
-            torch.load("mlcdft_params.dat", map_location=qp.rc.device)
-        )
-        cdft = hr.mlcdft.Minimizer(functional=functional, grid1d=grid1d, n_bulk=0.6)
-
-    qp.log.info(f"mu = {cdft.mu}")
-
-    # Set external potential:
-    V0 = 2 * cdft.T
+    # Create external potential:
+    V0 = 2 * T
     Vsigma = 0.1
-    cdft.V.data = (0.5 * V0) * (
-        (0.4 * grid1d.L - (grid1d.z - 0.5 * grid1d.L).abs()) / Vsigma
-    ).erfc()
+    V = qp.grid.FieldR(
+        grid1d.grid,
+        data=(0.5 * V0)
+        * ((0.4 * grid1d.L - (grid1d.z - 0.5 * grid1d.L).abs()) / Vsigma).erfc(),
+    )
 
-    # Finite difference test
-    # cdft.finite_difference_test(cdft.random_direction())
+    # Create exact functional:
+    cdfts = {}
+    cdfts["Exact"] = hr.HardRodsFMT(grid1d, R=0.5, T=T, n_bulk=n_bulk)
 
-    cdft.minimize()
-    n = cdft.n
+    # Create MLCDFT approximation:
+    functional = hr.mlcdft.Functional(
+        T=T,
+        w=hr.mlcdft.NNFunction(1, 2, [10, 10]),
+        f_ex=hr.mlcdft.NNFunction(2, 2, [10, 10]),
+    )
+    functional.load_state_dict(
+        torch.load("mlcdft_params.dat", map_location=qp.rc.device)
+    )
+    cdfts["ML"] = hr.mlcdft.Minimizer(
+        functional=functional, grid1d=grid1d, n_bulk=n_bulk
+    )
+
+    for cdft in cdfts.values():
+        cdft.V = V
+        cdft.finite_difference_test(cdft.random_direction())
+        cdft.minimize()
 
     # Plot density and potential:
     if qp.rc.is_head:
         z1d = hr.get1D(grid1d.z)
-        plt.plot(z1d, hr.get1D(cdft.V.data), label="V")
-        plt.plot(z1d, hr.get1D(n.data), label="n")
-        # plt.plot(z1d, get1D(n.convolve(w0_tilde).data), label="n0")
-        # plt.plot(z1d, get1D(n.convolve(w1_tilde).data), label="n1")
-        plt.axhline(cdft.n_bulk, color="k", ls="dotted")
+        plt.plot(z1d, hr.get1D(V.data), label="V")
+        for cdft_name, cdft in cdfts.items():
+            qp.log.info(f"mu ({cdft_name}) = {cdft.mu}")
+            plt.plot(z1d, hr.get1D(cdft.n.data), label=f"n ({cdft_name})")
+        plt.axhline(n_bulk, color="k", ls="dotted")
         plt.xlabel("z")
         plt.legend()
         plt.show()
