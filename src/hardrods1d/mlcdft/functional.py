Fixed gradient propagation through mu

src/hardrods1d/mlcdft/functional.py
@@ -39,15 +39,15 @@ class Functional(torch.nn.Module):  # type: ignore
         energy["Fex"] = self.T * (n_bar ^ self.f_ex(n_bar)).sum(dim=0)  # Excess part
         return energy
 
-    def get_mu(self, n_bulk: float) -> float:
+    def get_mu(self, n_bulk: float, create_graph: bool = False) -> torch.Tensor:
         """Compute chemical potential that will produce target density `n_bulk`."""
         # Bulk condition: (d/dn) [f_id(n) + f_ex(w * n)] = mu
         w_bulk = self.w(torch.zeros(1, device=qp.rc.device))
         n = torch.tensor(n_bulk)
         n.requires_grad = True
         n_bar = w_bulk * n
-        (self.T * (n * n.log() + n_bar @ self.f_ex(n_bar))).backward()
-        return n.grad.item()
+        energy_density = self.T * (n * n.log() + n_bar @ self.f_ex(n_bar))
+        return torch.autograd.grad(energy_density, n, create_graph=create_graph)[0]
 
     @staticmethod
     def Gmag(grid: qp.grid.Grid):

src/hardrods1d/mlcdft/minimizer.py
@@ -31,7 +31,7 @@ class Minimizer(qp.utils.Minimize[qp.grid.FieldR]):
         self.functional = functional
         self.grid1d = grid1d
         self.n_bulk = n_bulk
-        self.mu = functional.get_mu(n_bulk)
+        self.mu = functional.get_mu(n_bulk).item()
         self.T = functional.T
         self.logn = qp.grid.FieldR(
             grid1d.grid, data=torch.full_like(grid1d.z, np.log(self.n_bulk))

src/hardrods1d/mlcdft/trainer.py
@@ -59,12 +59,13 @@ class Trainer(torch.nn.Module):  # type: ignore
     def forward(self, data: hr.mlcdft.Data) -> torch.Tensor:
         """Compute loss function for one complete perturbation data-set"""
         # Set mu
-        mu = self.functional.get_mu(data.n_bulk)
+        mu = self.functional.get_mu(data.n_bulk, create_graph=True)
+        V_minus_mu = qp.grid.FieldR(data.V.grid, data=(data.V.data - mu))
 
         # Compute energy and gradient (= error in V):
         data.n.data.requires_grad = True
         data.n.data.grad = None
-        E = self.functional.get_energy(data.n, data.V - mu)
+        E = self.functional.get_energy(data.n, V_minus_mu)
         Verr = torch.autograd.grad(
             E.sum_tensor().sum(), data.n.data, create_graph=True
         )[0]
@@ -109,14 +110,15 @@ def main() -> None:
     qp.utils.log_config()  # default set up to log from MPI head alone
     qp.log.info("Using QimPy " + qp.__version__)
     qp.rc.init()
+    torch.random.manual_seed(0)
 
     # TODO: get all parameters from YAML input
 
     # Initialize functional:
     functional = hr.mlcdft.Functional(
         T=1.0,
-        w=hr.mlcdft.NNFunction(1, 2, [30, 30, 30]),
-        f_ex=hr.mlcdft.NNFunction(2, 2, [30, 30, 30]),
+        w=hr.mlcdft.NNFunction(1, 2, [10, 10]),
+        f_ex=hr.mlcdft.NNFunction(2, 2, [10, 10]),
     )
     params_filename = "mlcdft_params.dat"
     if os.path.isfile(params_filename):
@@ -127,11 +129,10 @@ def main() -> None:
     # Initialize trainer:
     filenames = sys.argv[1:]
     assert len(filenames)
-    torch.random.manual_seed(0)
-    trainer = Trainer(functional, filenames, batch_size=8)
-    optimizer = torch.optim.SGD(trainer.functional.parameters(), lr=3e-6)
+    trainer = Trainer(functional, filenames, batch_size=4)
+    optimizer = torch.optim.SGD(trainer.functional.parameters(), lr=1e-5)
 
-    epochs = 100
+    epochs = 1000
     for t in range(epochs):
         qp.log.info(f"\n------------- Epoch {t + 1} -------------")
         trainer.train_loop(optimizer)