10e66a153eb91cb032ba01d57816ae47cf3a981d
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2022-12-02 15:50:30 -0500

    Added rectangular potential parameterized by duty cycle and smoothing

--- src/hardrods1d/v_shape.py
+++ src/hardrods1d/v_shape.py
@@ -10,10 +10,9 @@ def get(grid1d: Grid1D, *, shape: str, **kwargs) -> qp.grid.FieldR:
 
 
 def _get_gauss(grid1d: Grid1D, *, sigma: float) -> torch.Tensor:
-    norm_fac = sigma * np.sqrt(2 * np.pi) / grid1d.L
-    trans_phase = (2j * np.pi) * grid1d.iGz * 0.5  # shift to center of unit cell
-    gauss_tilde = (-0.5 * (grid1d.Gmag * sigma).square() + trans_phase).exp() * norm_fac
-    return gauss_tilde
+    norm_fac = sigma * np.sqrt(2 * np.pi) / grid1d.L  # peak value at 1
+    translation_phase = (2j * np.pi) * grid1d.iGz * 0.5  # shift to center of unit cell
+    return (-0.5 * (grid1d.Gmag * sigma).square() + translation_phase).exp() * norm_fac
 
 
 def _get_cosine(grid1d: Grid1D, *, n: int = 1) -> torch.Tensor:
@@ -21,4 +20,16 @@ def _get_cosine(grid1d: Grid1D, *, n: int = 1) -> torch.Tensor:
     return torch.where(abs(iGz) == n, 0.5, 0.0).to(torch.complex128)
 
 
-_get_map: Dict[str, Any] = {"gauss": _get_gauss, "cosine": _get_cosine}
+def _get_rectangular(
+    grid1d: Grid1D, *, sigma: float = 0.0, duty: float = 0.5
+) -> torch.Tensor:
+    rect_tilde = torch.sinc(grid1d.iGz * duty) * duty
+    gauss_tilde = (-0.5 * (grid1d.Gmag * sigma).square()).exp()
+    return (rect_tilde * gauss_tilde).to(torch.complex128)
+
+
+_get_map: Dict[str, Any] = {
+    "gauss": _get_gauss,
+    "cosine": _get_cosine,
+    "rectangular": _get_rectangular,
+}
