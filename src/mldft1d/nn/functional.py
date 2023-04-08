6057b4102dee6d0eed42795097cb06f96387c3e5
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-04-08 14:51:20 -0400

    Switched to real space weight functions because Fourier oscillations causing much of the difficulty

--- src/mldft1d/nn/functional.py
+++ src/mldft1d/nn/functional.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 import qimpy as qp
+import numpy as np
+import functools
 import torch
 import os
 from mpi4py import MPI
-from typing import Optional
 from .function import Function
 
 
@@ -15,11 +16,9 @@ class Functional(torch.nn.Module):  # type: ignore
     n_irred: int  #: Number of irreducible scalar combinations of weighted densities
     w: Function  #: Weight functions defining spatial nonlocality
     f: Function  #: Per-particle free energy function of weighted densities
-    Gc: float  #: Reciprocal space cutoff on weight functions
+    rc: float  #: Real space cutoff on weight functions
     cache_w: bool  #: Whether to precompute weight functions (must be off for training)
-
     _cached_w_tilde: dict[qp.grid.Grid, torch.Tensor]  #: cached weight functions
-    _cached_w0: Optional[torch.Tensor]  #: cached G=0 component of weights
 
     def __init__(
         self,
@@ -28,7 +27,7 @@ class Functional(torch.nn.Module):  # type: ignore
         n_weights: tuple[int, int],
         w_hidden_sizes: list[int],
         f_hidden_sizes: list[int],
-        Gc: float,
+        rc: float,
         cache_w: bool = True,
     ) -> None:
         """Initializes functional with specified sizes (and random parameters)."""
@@ -39,12 +38,11 @@ class Functional(torch.nn.Module):  # type: ignore
         self.n_irred = n_weights_even + len(self.i_odd_pair)
         self.w = Function(1, sum(n_weights), w_hidden_sizes)
         self.f = Function(self.n_irred, self.n_irred, f_hidden_sizes)
-        self.Gc = Gc
+        self.rc = rc
         self.cache_w = cache_w
         if comm.size > 1:
             self.bcast_parameters(comm)
         self._cached_w_tilde = dict()
-        self._cached_w0 = None
 
     @classmethod
     def load(
@@ -84,7 +82,7 @@ class Functional(torch.nn.Module):  # type: ignore
             n_weights=self.n_weights,
             w_hidden_sizes=self.w.n_hidden,
             f_hidden_sizes=self.f.n_hidden,
-            Gc=self.Gc,
+            rc=self.rc,
             state=self.state_dict(),
         )
         if comm.rank == 0:
@@ -96,12 +94,11 @@ class Functional(torch.nn.Module):  # type: ignore
             w_tilde = self._cached_w_tilde[grid]
         else:
             # Compute weight:
-            gradient_z = grid.get_gradient_operator("H")[2, 0]  # 1 x Nz array
-            g_sq = torch.clamp(qp.utils.abs_squared(gradient_z / self.Gc), max=1.0)
-            w_tilde = self.w(g_sq) * (1.0 - g_sq).square()
+            w_tilde = get_weight_calculator(grid, self.rc)(self.w)
 
             # Make odd weights:
             w_tilde = w_tilde.to(torch.complex128)
+            gradient_z = grid.get_gradient_operator("H")[2, 0]  # 1 x Nz array
             w_tilde[self.n_weights[0] :] *= gradient_z
 
             if self.cache_w:
@@ -110,17 +107,6 @@ class Functional(torch.nn.Module):  # type: ignore
         # Fix broadcast dimensions:
         Nw, Nz = w_tilde.shape
         return w_tilde.view(Nw, *((1,) * (n_dim_tot - 2)), Nz)
-
-    def get_w0(self) -> torch.Tensor:
-        """Get G=0 component of first weight (rest all zero)."""
-        if self.cache_w and (self._cached_w0 is not None):
-            w0 = self._cached_w0
-        else:
-            Gzero = torch.zeros((1,), device=qp.rc.device)
-            w0 = self.w(Gzero)[: self.n_weights[0]]  # odd ones are zero at G=0
-            if self.cache_w:
-                self._cached_w0 = w0.detach()
-        return w0
 
     def get_energy(self, n: qp.grid.FieldR) -> torch.Tensor:
         # Compute weighted densities:
@@ -135,7 +121,7 @@ class Functional(torch.nn.Module):  # type: ignore
 
     def get_energy_bulk(self, n: torch.Tensor) -> torch.Tensor:
         n_bar = torch.zeros((self.n_irred,) + n.shape, device=n.device, dtype=n.dtype)
-        n_bar[: self.n_weights[0]] = torch.einsum("w, ... -> w...", self.get_w0(), n)
+        n_bar[: self.n_weights[0]] = n
         return (n_bar * self.f(n_bar)).sum(dim=0)
 
     def bcast_parameters(self, comm: MPI.Comm) -> None:
@@ -160,3 +146,48 @@ def get_pair_indices(N: int) -> torch.Tensor:
     index = torch.arange(N, dtype=torch.long, device=qp.rc.device)
     i, j = torch.where(index[:, None] <= index[None, :])
     return i * N + j
+
+
+@functools.cache
+def get_weight_calculator(grid: qp.grid.Grid, rc: float) -> WeightCalculatorR:
+    return WeightCalculatorR(grid, rc)
+
+
+class WeightCalculatorR:
+    """Real-space weight calculator."""
+
+    sup: int  #: supercell multiplier to accommodate range of kernel
+    dz: float  #: grid spacing
+    mine: slice  #: portion of recirpocal space to retain on present process
+    z_sup_by_rc_sq: torch.Tensor  #: (z/rc)^2 in supercell (input to NN weight function)
+    prefactor: torch.Tensor  #: weight function prefactor enforcing smooth falloff at rc
+
+    def __init__(self, grid: qp.grid.Grid, rc: float) -> None:
+        # Extract 1D grid properties:
+        L = grid.lattice.Rbasis[2, 2].item()
+        Nz = grid.shape[2]
+        dz = L / Nz
+
+        # Find supercell needed to accommodate rc:
+        sup = int(np.ceil(2 * rc / L))
+        Nz_sup = Nz * sup
+        iz_sup = torch.arange(Nz_sup, device=qp.rc.device)[None]  # extra dim for bcast
+        z_sup = dz * torch.where(iz_sup <= Nz_sup // 2, iz_sup, iz_sup - Nz_sup)
+
+        # Store required quantities:
+        self.sup = sup
+        self.dz = dz
+        self.mine = slice(grid.split2H.i_start, grid.split2H.i_stop)
+        self.z_sup_by_rc_sq = torch.clamp((z_sup / rc).square(), max=1.0)
+        self.prefactor = (1.0 - self.z_sup_by_rc_sq).square()  # C1 continuous at rc
+
+    def __call__(self, w: Function) -> torch.Tensor:
+        # Compute in real space (enforce positive, bounded and cutoff):
+        w_real = self.prefactor / (1.0 + w(self.z_sup_by_rc_sq).square())
+
+        # Convert to half reciprocal space and downselect to orginal cell:
+        w_tilde = (torch.fft.rfft(w_real).real * self.dz)[:, :: self.sup]
+
+        # Enforce G = 0 constraint:
+        w_zero = w_tilde[..., :1]  # G = 0 component of w_tilde (with same dimensions)
+        return w_tilde[..., self.mine] / w_zero
